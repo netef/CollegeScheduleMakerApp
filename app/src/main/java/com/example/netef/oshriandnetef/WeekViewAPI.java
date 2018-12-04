5fe4f06d7384e375bfa37053c9b1b09cbd2d90b4
Oshry Mahlev <oshri1992@gmail.com>
2018-12-04 18:51:33 +0200

    Week

--- app/src/main/java/com/example/netef/oshriandnetef/WeekViewAPI.java
+++ app/src/main/java/com/example/netef/oshriandnetef/WeekViewAPI.java
@@ -3,11 +3,15 @@ package com.example.netef.oshriandnetef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 public class WeekViewAPI extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week_view_api);
+        MaterialCalendarView calendarView =findViewById(R.id.calendarView);
+
     }
 }
